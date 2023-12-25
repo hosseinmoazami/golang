744303d98ed5502b0b7fefda7de4adf67e10b947
Hossein <hossein.moazami0662@gmail.com>
2023-12-25 13:19:23 +0330

    demo interface

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+	PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("chop salad")
+	fmt.Println("add dressing")
+}
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare Dishes:")
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("--Dish: %v--\v", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+
+}
+
+func main() {
+	dishes := []Preparer{Chicken("Chicken Wings"), Salad("Iceberg Salad")}
+	prepareDishes(dishes)
 }
