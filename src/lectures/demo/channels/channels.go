50e851ff8ca279ae144d84dcc41b0f0650640281
Hossein <hossein.moazami0662@gmail.com>
2023-12-26 16:04:54 +0330

    demo channel

--- src/lectures/demo/channels/channels.go
+++ src/lectures/demo/channels/channels.go
@@ -1,8 +1,65 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+type ControlMg int
+
+const (
+	DoExit = iota
+	ExitOk
+)
+
+type Job struct {
+	data int
+}
+
+type Result struct {
+	result int
+	job    Job
+}
+
+func doubler(jobs <-chan Job, results chan<- Result, control chan ControlMg) {
+	for {
+		select {
+		case msg := <-control:
+			switch msg {
+			case DoExit:
+				fmt.Println("Exit Goroutine")
+				control <- ExitOk
+				return
+			default:
+				panic("Unhandled Control Message")
+			}
+		case job := <-jobs:
+			results <- Result{result: job.data * 2, job: job}
+		}
+	}
+}
 
 func main() {
+	jobs := make(chan Job, 50)
+	results := make(chan Result, 50)
+	control := make(chan ControlMg)
 
+	go doubler(jobs, results, control)
+
+	for i := 0; i < 30; i++ {
+		jobs <- Job{i}
+	}
+
+	for {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		case <-time.After(500 * time.Microsecond):
+			fmt.Println("Time out")
+			control <- DoExit
+			<-control
+			fmt.Println("Program Exit")
+			return
+		}
+	}
 }
