02bdcfbbceecb463a7a31474ae2c898aecbde0b3
Hossein <hossein.moazami0662@gmail.com>
2023-12-26 14:25:44 +0330

    demo goroutin

--- src/lectures/demo/goroutines/goroutines.go
+++ src/lectures/demo/goroutines/goroutines.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
-import "time"
-import "unicode"
+import (
+	"fmt"
+	"time"
+	"unicode"
+)
 
 func main() {
 	data := []rune{'a', 'b', 'c', 'd'}
+	var capitalized []rune
+
+	capIt := func(r rune) {
+		capitalized = append(capitalized, unicode.ToUpper(r))
+		fmt.Println()
+		fmt.Printf("%c Done!\n", r)
+		fmt.Println()
+	}
+
+	for i := 0; i < len(data); i++ {
+		go capIt(data[i])
+		go hugeCalculation("Hello mamesita mamaesima caltuera")
+	}
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Printf("Capitalized: %c\n", capitalized)
+}
+
+func hugeCalculation(text string) {
+	for k, v := range text {
+		fmt.Printf("[%c] is the char [%v] in text.\n", v, k)
+	}
 }
