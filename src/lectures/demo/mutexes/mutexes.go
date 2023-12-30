ffe6989b7216f3233f0ce45cb6792ca42b143c45
Hossein <hossein.moazami0662@gmail.com>
2023-12-30 13:32:12 +0330

    demo synchronization

--- src/lectures/demo/mutexes/mutexes.go
+++ src/lectures/demo/mutexes/mutexes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,6 +11,38 @@ func wait() {
 	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 }
 
+type Hits struct {
+	count int
+	sync.Mutex
+}
+
+func serve(wg *sync.WaitGroup, hits *Hits, iteration int) {
+	wait()
+	hits.Lock()
+	defer hits.Unlock()
+	defer wg.Done()
+	hits.count += 1
+	fmt.Println("served iteration", iteration)
+}
+
 func main() {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var wg sync.WaitGroup
+
+	hitsCounter := Hits{}
+	for i := 0; i < 5000; i++ {
+		iteration := i
+		wg.Add(1)
+		go serve(&wg, &hitsCounter, iteration)
+	}
+
+	fmt.Printf("waiting for goroutines...\n\n")
+	wg.Wait()
+
+	hitsCounter.Lock()
+	totalHits := hitsCounter.count
+	hitsCounter.Unlock()
+
+	fmt.Printf("\ntotal Hits:%d\n", totalHits)
 }
--- src/lectures/demo/waitgroups/waitgroups.go
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		counter += 1
+		go func() {
+			defer func() {
+				fmt.Println(counter, "goroutines remaining")
+				counter -= 1
+				wg.Done()
+			}()
+			duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+			fmt.Println("Waiting for", duration)
+			time.Sleep(duration)
+		}()
+	}
+	fmt.Println("Waiting for goroutines to finish")
+	wg.Wait()
+	fmt.Println("Done!")
 }
