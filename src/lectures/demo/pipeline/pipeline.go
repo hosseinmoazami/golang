dc3552458d1e10c29335043a87c2bb91c9e71209
Hossein <hossein.moazami0662@gmail.com>
2023-12-31 15:37:53 +0330

    demo pipeline pattern

--- src/lectures/demo/pipeline/pipeline.go
+++ src/lectures/demo/pipeline/pipeline.go
@@ -1,6 +1,81 @@
 package main
 
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/chai2010/webp"
+	"github.com/google/uuid"
+)
+
+func base64ToRawImage(base64Img string) image.Image {
+	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(base64Img))
+
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return img
+}
+
+func encodeToWebp(img image.Image) bytes.Buffer {
+	var buf bytes.Buffer
+	if err := webp.Encode(&buf, img, &webp.Options{Lossless: true}); err != nil {
+		log.Fatal(err)
+	}
+	return buf
+}
+
+func saveToDisk(imgBuf bytes.Buffer) string {
+	filename := fmt.Sprintf("%v.webp", uuid.New().String())
+	os.WriteFile(filename, imgBuf.Bytes(), 0644)
+	return filename
+}
+
+func makeWork(base64Images ...string) <-chan string {
+	out := make(chan string)
+	go func() {
+		for _, encodedImg := range base64Images {
+			out <- encodedImg
+		}
+		close(out)
+	}()
+	return out
+}
+
+func pipeline[I any, O any](input <-chan I, process func(I) O) <-chan O {
+	out := make(chan O)
+	go func() {
+		for in := range input {
+			out <- process(in)
+		}
+		close(out)
+	}()
+	return out
+}
+
 func main() {
+	// load data into pipeline
+	base64Images := makeWork(img1, img2, img3)
+
+	// decode base64 into image format
+	rawImages := pipeline(base64Images, base64ToRawImage)
+	// encode as webp
+	webpImages := pipeline(rawImages, encodeToWebp)
+	// save images to disk
+	filenames := pipeline(webpImages, saveToDisk)
+
+	for name := range filenames {
+		fmt.Println(name)
+	}
 }
 
 const img1 = `
--- src/lectures/go.mod
+++ src/lectures/go.mod
@@ -1,3 +1,8 @@
 module coursecontent
 
 go 1.21
+
+require (
+	github.com/chai2010/webp v1.1.1
+	github.com/google/uuid v1.5.0
+)
